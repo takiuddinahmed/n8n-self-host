c29209ceba54dfa8e3e5351c5e6fb723f0715412
Takiuddin Ahmed <takiuddinahmed@gmail.com>
2025-10-21 05:52:41 +0600

    fix: change os to centos

--- command.sh
+++ command.sh
@@ -12,26 +12,40 @@ echo "🚀 Starting n8n self-hosting setup..."
 # fi
 
 # Remove incompatible or out of date Docker implementations if they exist
-echo "🧹 Removing old Docker packages..."
-for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do 
-    sudo apt-get remove -y $pkg 2>/dev/null || true
-done
+# echo "🧹 Removing old Docker packages..."
+# for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do 
+#     sudo apt-get remove -y $pkg 2>/dev/null || true
+# done
 
-# Install prereq packages
-echo "📦 Installing prerequisites..."
-sudo apt-get update -y && sudo apt-get upgrade -y
-sudo apt-get install -y ca-certificates curl gnupg lsb-release
-# Download the repo signing key
-sudo install -m 0755 -d /etc/apt/keyrings
-sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-sudo chmod a+r /etc/apt/keyrings/docker.asc
-# Configure the repository
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+# # Install prereq packages
+# echo "📦 Installing prerequisites..."
+# sudo apt-get update -y && sudo apt-get upgrade -y
+# sudo apt-get install -y ca-certificates curl gnupg lsb-release
+# # Download the repo signing key
+# sudo install -m 0755 -d /etc/apt/keyrings
+# sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+# sudo chmod a+r /etc/apt/keyrings/docker.asc
+# # Configure the repository
+# echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-# Update and install Docker and Docker Compose
-sudo apt-get updat -y && sudo apt-get upgrade -y
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+# # Update and install Docker and Docker Compose
+# sudo apt-get updat -y && sudo apt-get upgrade -y
+# sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 
+
+sudo dnf remove docker \
+                  docker-client \
+                  docker-client-latest \
+                  docker-common \
+                  docker-latest \
+                  docker-latest-logrotate \
+                  docker-logrotate \
+                  docker-engine -y
+
+sudo dnf -y install dnf-plugins-core
+sudo dnf config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+
+sudo dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 
 
 # Add user to docker group
