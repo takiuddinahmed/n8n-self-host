56d1f52238d0cb07d8c22472cb832eaa6bbd192b
Takiuddin Ahmed <takiuddinahmed@gmail.com>
2025-10-21 05:29:22 +0600

    fix: always allow in update

--- command.sh
+++ command.sh
@@ -19,7 +19,7 @@ done
 
 # Install prereq packages
 echo "📦 Installing prerequisites..."
-sudo apt-get update
+sudo apt-get update -y && sudo apt-get upgrade -y
 sudo apt-get install -y ca-certificates curl gnupg lsb-release
 # Download the repo signing key
 sudo install -m 0755 -d /etc/apt/keyrings
@@ -29,8 +29,8 @@ sudo chmod a+r /etc/apt/keyrings/docker.asc
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 # Update and install Docker and Docker Compose
-sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get updat -y && sudo apt-get upgrade -y
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 
 
 
